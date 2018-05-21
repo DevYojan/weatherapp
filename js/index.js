187351304edfd6a1f96623c2648066a359279bea
Yojan Regmi <devyojan@gmail.com>
2018-05-21 18:48:07 +0545

    Adding files

---
+++ js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	
+	if (navigator.geolocation) {
+		
+		navigator.geolocation.getCurrentPosition(function(position) {
+
+			$.getJSON('https://fcc-weather-api.glitch.me/api/current?lat='+
+			position.coords.latitude+'&lon='+position.coords.longitude, function(json){
+				console.log(json);
+				$('#location').html(json.name + ', ' + json.sys.country);
+				$('#icon').html("<img src="+json.weather[0].icon+'" id="image">');
+				$('#temp').html("<span id='degree'>" + json.main.temp + "</span> &deg; "
+					+"<span id='convert'>C</span>");
+
+				/*Captizlizing first letter of each word of description*/
+				
+				json.weather[0].description =  json.weather[0].description.replace(/\w\S*/g, 
+				function(txt){
+					return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+				});
+
+				$('#description').html("<br>" + json.weather[0].description);
+
+				$('#convert').on('click', function(){
+					var c = json.main.temp;
+
+					var text = $('#convert').text();
+
+					if (text == "C") {
+						var f = (json.main.temp * (9/5))+32;
+						$('#degree').html(f);
+						$('#convert').html("F");
+					} else {
+						$('#degree').html(c);
+						$('#convert').html("C");
+					}
+
+				});
+			});
+		});
+	} else {
+		$('#message').html("Please allow the application to access your location.");
+	}
+});
